a8ba1d6497b8bddad378cc8a422ca6ba86e9457f
Kaio-Nguyen <nhatkha06299@gmail.com>
2024-05-25 20:43:02 +0700

    create model comment

---
+++ src/model/comment.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    postId: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
+    numberOfLikes: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Comment", commentSchema);
