ec22b9b1c8f7edc4216bbef21e5c02c055838dfc
Kaio-Nguyen <nhatkha06299@gmail.com>
2024-05-26 14:38:57 +0700

    add and create: feature comment

---
+++ client/src/components/Comment/CommentSection.jsx
@@ -0,0 +1,230 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import apiList from "../../api/apiList";
+
+import Comment from "./Comment";
+
+export default function CommentSection({ postId }) {
+  const { user, post } = useSelector((state) => ({
+    user: state.auth.authData,
+    post: state.post.posts,
+  }));
+
+  console.log("user:", user.token);
+
+  const navigate = useNavigate();
+
+  const [comment, setComment] = useState("");
+  const [commentError, setCommentError] = useState(null);
+  const [comments, setComments] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [commentToDelete, setCommentToDelete] = useState(null);
+
+  const [mainUser, setMainUser] = useState([]);
+
+  useEffect(() => {
+    const user = async () => {
+      try {
+        const res = await fetch(apiList.getUser);
+        if (res.ok) {
+          const data = await res.json();
+          setMainUser(data.users);
+        }
+      } catch (error) {
+        console.log({ error });
+      }
+    };
+    user();
+  }, []);
+
+  // console.log("mainUser:", mainUser);
+
+  useEffect(() => {
+    const getComments = async () => {
+      try {
+        const res = await fetch(`${apiList.getPostComment}${postId}`);
+        if (res.ok) {
+          const data = await res.json();
+          setComments(data);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getComments();
+  }, [postId]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (comment.length > 200) {
+      return;
+    }
+    try {
+      const res = await fetch(apiList.create, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${user.token}`,
+        },
+        body: JSON.stringify({
+          content: comment,
+          postId,
+          userId: user.user._id,
+        }),
+      });
+      const data = await res.json();
+      if (res.ok) {
+        setComment("");
+        setCommentError(null);
+        setComments([data, ...comments]);
+      } else {
+        setCommentError(data.message || "Failed to create comment.");
+      }
+    } catch (error) {
+      setCommentError(error.message);
+    }
+  };
+
+  const handleLike = async (commentId) => {
+    try {
+      if (!user) {
+        navigate("/sign-in");
+        return;
+      }
+      const res = await fetch(`${apiList.likeComment}${commentId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setComments(
+          comments.map((comment) =>
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: data.likes,
+                  numberOfLikes: data.likes.length,
+                }
+              : comment
+          )
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const handleEdit = async (comment, editedContent) => {
+    setComments(
+      comments.map((c) =>
+        c._id === comment._id ? { ...c, content: editedContent } : c
+      )
+    );
+  };
+
+  const handleDelete = async (commentId) => {
+    setShowModal(false);
+    try {
+      if (!user) {
+        navigate("/sign-in");
+        return;
+      }
+      const res = await fetch(`${apiList.deleteComment}${commentId}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+          "Content-Type": "application/json",
+        },
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setComments(comments.filter((comment) => comment._id !== commentId));
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const newMainUser = mainUser.filter((user) => user._id === post.userId);
+
+  return (
+    <>
+      {comments.length === 0 ? (
+        <div className="text-black p-4 antialiased flex">
+          <p className="text-sm my-5">No comments yet!</p>
+        </div>
+      ) : (
+        <div className="text-black p-4 antialiased flex">
+          <div className="text-sm my-5 flex items-center gap-1">
+            <p>Comments</p>
+            <div className="border border-gray-400 py-1 px-2 rounded-sm">
+              <p>{comments.length}</p>
+            </div>
+          </div>
+          {comments.map((comment) => (
+            <Comment
+              key={comment._id}
+              comment={comment}
+              onLike={handleLike}
+              onEdit={handleEdit}
+              onDelete={(commentId) => {
+                setShowModal(true);
+                setCommentToDelete(commentId);
+              }}
+            />
+          ))}
+        </div>
+      )}
+      {user && user.user && (
+        <form
+          onSubmit={handleSubmit}
+          className="relative flex items-center self-center w-full max-w-xl p-4 overflow-hidden text-gray-600 focus-within:text-gray-400"
+        >
+          <img
+            className="w-10 h-10 object-cover rounded-full shadow mr-2 cursor-pointer"
+            alt="User avatar"
+            src={
+              !user.profilePicture
+                ? user.profilePicture
+                : "data:image/svg+xml,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg' aria-hidden='true' role='presentation' focusable='false' class='block h-full w-full fill-current'%3E%3Cpath d='m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z'/%3E%3C/svg%3E"
+            }
+          />
+          <span className="absolute inset-y-0 right-0 flex items-center pr-6">
+            <button
+              type="submit"
+              className="p-1 focus:outline-none focus:shadow-none hover:text-blue-500"
+            >
+              <svg
+                className="w-6 h-6 transition ease-out duration-300 hover:text-blue-500 text-gray-400"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                ></path>
+              </svg>
+            </button>
+            <button type="submit">Submit</button>
+          </span>
+          <input
+            type="search"
+            className="w-full rounded-[25px] py-2 pl-4 pr-10 text-sm bg-gray-100 border border-transparent appearance-none rounded-tg placeholder-gray-400"
+            placeholder="Post a comment..."
+            autocomplete="off"
+            onChange={(e) => setComment(e.target.value)}
+            value={comment}
+          />
+        </form>
+      )}
+    </>
+  );
+}
