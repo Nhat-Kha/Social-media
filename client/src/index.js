a4dea94506d9c3d12834cc4826702e6c4a3611ee
Kaio-Nguyen <nhatkha06299@gmail.com>
2024-05-27 22:33:55 +0700

    delete react.strictmode

--- client/src/index.js
+++ client/src/index.js
@@ -7,13 +7,11 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="*" element={<App />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 );
