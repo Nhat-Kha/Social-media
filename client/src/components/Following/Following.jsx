91058111898025485107f911596513f40aef3006
Kaio-Nguyen <nhatkha06299@gmail.com>
2024-05-31 19:39:16 +0700

    update interface follower and following for user

--- client/src/components/Following/Following.jsx
+++ client/src/components/Following/Following.jsx
@@ -4,45 +4,67 @@ import { userSelector } from "../../redux/Selector/Selector";
 import apiList from "../../api/apiList";
 import { followUser, unfollowUser } from "../../redux/actions/UserAction";
 import User from "./User/User";
+import FollowModal from "./FollowModal/FollowModal";
 
-export default function Following() {
+export default function Following({ location }) {
   const user = useSelector(userSelector);
+  // console.log(user);
 
   const [modal, setModal] = useState(false);
   const [mainUser, setMainUser] = useState([]);
 
   useEffect(() => {
-    const user = async () => {
+    const users = async () => {
       try {
         const res = await fetch(apiList.getUser);
         if (res.ok) {
           const data = await res.json();
+          console.log("data:", data.users);
+          const filterUser = data.users.filter((u) => u._id !== user.user._id);
 
-          setMainUser(data.users);
+          setMainUser(filterUser);
+          // console.log({mainUser});
         }
       } catch (error) {
         console.log({ error });
       }
     };
-    user();
-  }, []);
+    users();
+  }, [user]);
 
   const newMainUser = mainUser.slice(0, 3);
-
-  console.log("mainUser id:", newMainUser);
 
   return (
     <div className="bg-white shadow mt-4 rounded-lg p-4">
       <h3 className="text-gray-600 text-sm font-semibold mb-4">Following</h3>
-      {newMainUser.length > 0 &&
-        newMainUser.map((use) => {
-          if (use._id !== user._id) {
-            return <User use={use} key={use._id} />;
-          } else {
-            return null;
-          }
-        })}
-      <span className="flex justify-center items-center">Show More</span>
+      {!location
+        ? newMainUser.length > 0 &&
+          newMainUser.map((use) => {
+            if (use._id !== user._id) {
+              return <User use={use} key={use._id} />;
+            } else {
+              return null;
+            }
+          })
+        : mainUser.length > 0 &&
+          mainUser.map((use) => {
+            if (use._id !== user._id) {
+              return <User use={use} key={use._id} />;
+            } else {
+              return null;
+            }
+          })}
+      {!location ? (
+        <span
+          onClick={() => setModal(true)}
+          className="flex justify-center items-center cursor-pointer"
+        >
+          Show More
+        </span>
+      ) : (
+        ""
+      )}
+      <FollowModal modalOpened={modal} setModalOpened={setModal} />
     </div>
   );
 }
--- client/src/components/Following/User/User.jsx
+++ client/src/components/Following/User/User.jsx
@@ -6,15 +6,13 @@ import { followUser, unfollowUser } from "../../../redux/actions/UserAction";
 export default function User({ use }) {
   const dispatch = useDispatch();
   const user = useSelector(userSelector);
-  console.log("use:", use);
-  console.log("user:", user);
 
   const userId = user?.user._id;
-  console.log({ userId });
+  // console.log({ userId });
   const initialFollow = use?.followers || [];
 
   const [following, setFollowing] = useState(initialFollow.includes(userId));
-  console.log({ initialFollow });
+  // console.log({ initialFollow });
 
   const handleFollow = () => {
     console.log("start mainUser id:", use);
@@ -25,37 +23,45 @@ export default function User({ use }) {
     setFollowing((prev) => !prev);
   };
 
-  console.log({ following });
+  // console.log({ following });
 
   return (
     <>
-      <ul className="flex flex-col items-start justify-center space-x-2">
-        <li className="flex flex-row items-center space-y-2">
-          <a className="block bg-white p-1 rounded-full" href="#!">
-            <img
-              alt=""
-              className="w-16 rounded-full"
-              src={
-                !use.profilePicture
-                  ? "data:image/svg+xml,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg' aria-hidden='true' role='presentation' focusable='false' className='block h-full w-full fill-current'%3E%3Cpath d='m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z'/%3E%3C/svg%3E"
-                  : use.profilePicture
+      <ul className="flex flex-col items-start justify-center space-x-2 w-full">
+        <li className="w-full">
+          <div className="flex justify-between items-center">
+            <div className="flex flex-row justify-center items-center">
+              <a className="block bg-white p-1 rounded-full" href="#!">
+                <img
+                  alt=""
+                  className="w-16 rounded-full"
+                  src={
+                    !use.profilePicture
+                      ? "data:image/svg+xml,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg' aria-hidden='true' role='presentation' focusable='false' className='block h-full w-full fill-current'%3E%3Cpath d='m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z'/%3E%3C/svg%3E"
+                      : use.profilePicture
+                  }
+                />
+              </a>
+              <div className="flex flex-col cursor-default">
+                <span className="text-sm text-gray-500 font-medium">
+                  {use.firstname}
+                </span>
+                <span className="text-base text-black font-semibold">
+                  {use.username}
+                </span>
+              </div>
+            </div>
+            <button
+              className={
+                following
+                  ? "h-[2rem] w-[8rem] transition duration-200 font-medium px-[20px] border-2 bg-gray-400 hover:bg-gray-500 text-white hover:text-gray-50 text-md rounded-md cursor-pointer"
+                  : "h-[2rem] w-[8rem] transition duration-200 font-medium px-[20px] bg-green-300 text-white rounded-md hover:bg-green-500"
               }
-            />
-          </a>
-          <div className="flex flex-col">
-            <span className="text-base text-black">{use.username}</span>
-            <span className="text-base text-black">{use.username}</span>
+              onClick={handleFollow}
+            >
+              {following ? "Unfollow" : "Follow"}
+            </button>
           </div>
-          <button
-            className={
-              following
-                ? "h-[2rem] px-[20px] border-2 bg-orange-300 cursor-pointer"
-                : "h-[2rem] px-[20px]"
-            }
-            onClick={handleFollow}
-          >
-            {following ? "Unfollow" : "Follow"}
-          </button>
         </li>
       </ul>
     </>
