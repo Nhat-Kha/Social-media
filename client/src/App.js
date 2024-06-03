a6a6838d98f55dbdf68e62f0f903f876042a3470
Kaio-Nguyen <nhatkha06299@gmail.com>
2024-06-03 18:48:47 +0700

    open feature loading

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,40 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Auth from "./pages/Auth/Auth";
 import { useSelector } from "react-redux";
 import Home from "./pages/Home";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/Layout/Navbar/Navbar";
 import Loading from "./components/container/LoadingPage";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Profile from "./pages/Profile/Profile";
 import Chat from "./pages/Chat/Chat";
+import { io } from "socket.io-client";
+import Layout from "./components/Layout/Layout";
+import Search from "./components/Search/Search";
 
 export default function App() {
+  // const socket = useRef();
+
   const user = useSelector((state) => state.auth.authData);
-  // const [loading, setLoading] = useState(true);
+
+  // const [onlineUsers, setOnlineUsers] = useState([]);
+
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 3500);
+  }, []);
+  if (loading) {
+    return <Loading />;
+  }
+
   // useEffect(() => {
-  //   setTimeout(() => setLoading(false), 4000);
-  // }, []);
-  // if (loading) {
-  //   return <Loading />;
-  // }
+  //   const url = "http://localhost:3300";
+  //   console.log(`Connecting to WebSocket server at ${url}`);
+  //   socket.current = io(url);
+  //   console.log(`WebSocket connected: ${socket.current}`);
+  //   socket.current.emit("new-user-add", user._id);
+  //   socket.current.on("get-users", (user) => {
+  //     setOnlineUsers(user);
+  //   });
+  // }, [user._id]);
   return (
     <div
       className="App bg-gray-50 pt-20 min-h-screen"
@@ -27,43 +46,46 @@ export default function App() {
             : "auto",
       }}
     >
-      <Navbar />
-      <Routes>
-        <Route
-          exact
-          path="/"
-          element={user ? <Navigate to="home" /> : <Navigate to="auth" />}
-        />
-        <Route
-          path="/home"
-          element={user ? <Home /> : <Navigate to="../auth" />}
-        />
-        <Route
-          exact
-          path="/auth"
-          element={user ? <Navigate to="../home" /> : <Auth />}
-        />
-        <Route
-          exact
-          path="/chat"
-          element={user ? <Chat /> : <Navigate to="../home" />}
-        />
-        <Route
-          exact
-          path="/profile/:id"
-          element={user ? <Profile /> : <Navigate to="../auth" />}
-        />
+      <Layout>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={user ? <Navigate to="home" /> : <Navigate to="auth" />}
+          />
+          <Route
+            path="/home"
+            element={user ? <Home /> : <Navigate to="../auth" />}
+          />
+          <Route
+            exact
+            path="/auth"
+            element={user ? <Navigate to="../home" /> : <Auth />}
+          />
+          <Route
+            exact
+            path="/chat"
+            element={user ? <Chat /> : <Navigate to="../home" />}
+          />
+          <Route
+            exact
+            path="/profile/:id"
+            element={user ? <Profile /> : <Navigate to="../auth" />}
+          />
+          <Route />
+          <Route exact path="/search" element={<Search />} />
 
-        <Route
-          exact
-          path="*"
-          element={
-            <main style={{ padding: "1rem" }}>
-              <p>There's nothing here!</p>
-            </main>
-          }
-        />
-      </Routes>
+          <Route
+            exact
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+        </Routes>
+      </Layout>
     </div>
   );
 }
